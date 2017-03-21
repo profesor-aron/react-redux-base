3a3d6de05ccd574196e7c49e417c181694768fcb
Aron Herrera <aronherrera2009@gmail.com>
2017-03-21 21:51:34 +0100

    Init project React Redux Base

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>Hello profesor Aron with Redux</h1>
+      </div>);
+  }
+}
